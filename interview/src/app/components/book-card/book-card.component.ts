40730a1a7a427db5ae5c69a44e9a4a4a8462c2d3
Jason Hornsby <jason.hornsby@scale.eu>
2024-11-05 11:06:16 +0100

    Added linter

--- interview/src/app/components/book-card/book-card.component.ts
+++ interview/src/app/components/book-card/book-card.component.ts
@@ -1,15 +1,29 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
-import { Book } from "../../models/book";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { Book, BookGenre } from "../../models/book";
 
 @Component({
   selector: 'app-book-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatChipsModule, MatExpansionModule, MatListModule, MatIconModule],
   templateUrl: './book-card.component.html',
   styleUrls: ['./book-card.component.scss']
 })
 export class BookCardComponent {
-  @Input() book: Book | undefined;
+  @Input() book!: Book;
+
+  BookGenre = BookGenre;
+
+  getGenreString(genre: BookGenre): string {
+    return BookGenre[genre].charAt(0).toUpperCase() + BookGenre[genre].slice(1);
+  }
+
+  formatDate(date: Date): string {
+    return new Date(date).toLocaleDateString();
+  }
 }
--- interview/src/app/services/book.service.ts
+++ interview/src/app/services/book.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, tap } from "rxjs";
+import { Observable, tap } from "rxjs";
 import { getLibraryDs, getStoreDs } from "../mocks/book.ds.mock";
 import { Book } from "../models/book";
 
@@ -10,13 +10,11 @@ export class BookService {
   libraryDs = getLibraryDs();
   storeDs = getStoreDs();
 
-  constructor() { }
-
   public getStoreBooks(): Observable<Book[]> {
     return this.storeDs.pipe(tap(result => console.log(result)));
   }
 
-  public getLibraryBooks(name?: string): Observable<Book[]> {
+  public getLibraryBooks(): Observable<Book[]> {
     return this.libraryDs;
   }
 }
